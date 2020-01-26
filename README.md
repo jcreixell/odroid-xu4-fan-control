95aa3bd64e3bf062a09595e54d50bf9088c51ea6
jcreixell <jcreixell@gmail.com>
2020-01-26 11:50:02 +0100

    Adapt to Lakka / LibreELEC
      - Replaces init script with systemd unit
      - Adapts script to busybox (some commands are not available in
        busybox's bash, or do not work as expected).
      - Tweaks fan speed so that it is always on for low temperatures, at a
        low speed, since Lakka consumes enough resources to have the fan
        start every few seconds. A white background noise is preferrable
        over an intermittent one.
      - Updated doc

--- README.md
+++ README.md
@@ -1,41 +1,38 @@
-### This is for the latest 4.y.y XU4 kernel as of April 2018
-
-If you are running the 3.y.y kernel then please use the master branch
+### Adapted for Lakka / LibreELEC
 
 ### What does it do?
 
-Makes Odroid XU4 fan silent on idle load and quite on medium load
+Allows controlling the fan speed for the Odroid XU4, by setting different fan speeds for different temperature
+thresholds.
 
-### Is it safe to use?
-
-When script quits it brings fan mode back to *automatic* "factory" settings.
+Out of the box, it sets the fan always on at a low speed, increaing the fan speed as temperature increases. I personally
+find this less disturbing as having the fan start and
+stop every few seconds. Feel free to change this to whatever is convenient for you.
 
 ## Usage
-``` 
-sudo apt-get install git
-git clone https://github.com/f1vefour/odroid-xu4-fan-control.git
-cd odroid-xu4-fan-control
+
+```bash
+// Make sure the script is executable
 chmod +x odroid-xu4-fan-control.sh
-sudo ./odroid-xu4-fan-control.sh
+
+// Run it (as root)
+./odroid-xu4-fan-control.sh
 ```
+
 ## Installation
 
-To make it start when system boots:
+```bash
+mkdir /storage/fan-control
+cp odroid-xu4-fan-control.sh /storage/fan-control/
+cp fan-control.service /storage/.config/system.d/
+systemctl daemon-reload
 
-edit odroid-fan-controller and add the path of the odroid-xu4-fan-control.sh script (full-pathname), then do the following to add it
-to the runlevels
+// Start the service
+systemctl start fan-control
 
-    cd /etc/init.d/
-    #adjust to correct, absolute path below
-    sudo ln -s ~/odroid-xu4-fan-control /etc/init.d/odroid-fan-controller
-    sudo update-rc.d odroid-fan-controller defaults
+// Check that it is running
+systemctl status fan-control
 
-you can also use the following to start the controller
-
-    sudo /etc/init.d/odroid-fan-controller start
-
-or
-
-    sudo /etc/init.d/odroid-fan-controller stop
-
-to stop the controller
+// Run the script at boot
+systemctl enable fan-control
+```
--- odroid-xu4-fan-control.sh
+++ odroid-xu4-fan-control.sh
@@ -8,19 +8,12 @@
 #set to false to suppress logs
 DEBUG=false
 
-# Make sure only root can run our script
-if (( $EUID != 0 )); then
-   echo "This script must be run as root:" 1>&2
-   echo "sudo $0" 1>&2
-   exit 1
-fi
-
 
 TEMPERATURE_FILE="/sys/devices/virtual/thermal/thermal_zone0/temp"
 FAN_MODE_FILE="/sys/devices/platform/pwm-fan/hwmon/hwmon0/automatic"
 FAN_SPEED_FILE="/sys/devices/platform/pwm-fan/hwmon/hwmon0/pwm1"
-TEST_EVERY=3 #seconds
-new_fan_speed_default=80
+TEST_EVERY="3" #seconds
+new_fan_speed_default="100"
 LOGGER_NAME=odroid-xu4-fan-control
 
 #make sure after quiting script fan goes to auto control
@@ -50,30 +43,30 @@ echo "sudo tail -f /var/log/syslog"
 
 while [ true ];
 do
-  echo 0 > ${FAN_MODE_FILE} #to be sure we can manage fan
+  echo "0" > ${FAN_MODE_FILE} #to be sure we can manage fan
 
   current_max_temp=`cat ${TEMPERATURE_FILE} | cut -d: -f2 | sort -nr | head -1`
   ${DEBUG} && logger -t $LOGGER_NAME "event: read_max; temp: ${current_max_temp}"
 
-  new_fan_speed=0
-  if (( ${current_max_temp} >= 75000 )); then
-    new_fan_speed=255
-  elif (( ${current_max_temp} >= 70000 )); then
-    new_fan_speed=200
-  elif (( ${current_max_temp} >= 68000 )); then
-    new_fan_speed=130
-  elif (( ${current_max_temp} >= 66000 )); then
-    new_fan_speed=80 
-  elif (( ${current_max_temp} >= 63000 )); then
-    new_fan_speed=80
-  elif (( ${current_max_temp} >= 60000 )); then
-    new_fan_speed=0
-  elif (( ${current_max_temp} >= 58000 )); then
-    new_fan_speed=0
-  elif (( ${current_max_temp} >= 55000 )); then
-    new_fan_speed=0
+  new_fan_speed="100"
+  if [ $current_max_temp -gt "75000" ]; then
+    new_fan_speed="255"
+  elif [ $current_max_temp -gt "70000" ]; then
+    new_fan_speed="240"
+  elif [ $current_max_temp -gt "68000" ]; then
+    new_fan_speed="100"
+  elif [ $current_max_temp -gt "66000" ]; then
+    new_fan_speed="100"
+  elif [ $current_max_temp -gt "63000" ]; then
+    new_fan_speed="100"
+  elif [ $current_max_temp -gt "60000" ]; then
+    new_fan_speed="100"
+  elif [ $current_max_temp -gt "58000" ]; then
+    new_fan_speed="100"
+  elif [ $current_max_temp -gt "55000" ]; then
+    new_fan_speed="100"
   else
-    new_fan_speed=0
+    new_fan_speed="100"
   fi
   ${DEBUG} && logger -t $LOGGER_NAME "event: adjust; speed: ${new_fan_speed}"
   echo ${new_fan_speed} > ${FAN_SPEED_FILE}
